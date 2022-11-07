f75339053845afc4fe438475c953256f9d2819ef
Veerash Palanichamy <veeresh.532000@gmail.com>
2022-11-07 11:03:30 -0500

    adding backend framework setup

---
+++ src/server/src/main/java/com/capstoners/Undertree/healthCheck/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.capstoners.Undertree.healthCheck;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+	}
+
+}
+            
