66a8edabc13888948a2e396ae346d0fd9b0ea60c
Kevin Kannammalil <kevinrk2000@gmail.com>
2022-11-08 23:54:34 -0500

    added login button

--- src/client/src/Pages/Login.js
+++ src/client/src/Pages/Login.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+function signIn() {
+  console.log('signing in');
+  window.open("http://localhost:5000/auth/github", "_self");
+}
+
 function Login() {
-  return <h2>Login</h2>;
+  return (
+    <div> 
+      <h2>Login</h2>
+      <a href="https://google.com" rel="noreferrer">
+        <button onClick={signIn}>
+          Login with GitHub
+        </button>
+      </a>
+    </div>
+
+  );
 }
 
 export default Login
