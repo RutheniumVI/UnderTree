7c932464f2ed5d741bfbe512b7537fd700338dd2
fahmed8383 <fahmed4030@gmail.com>
2023-01-30 21:15:18 -0500

    added database util and npm run start command

--- src/server/dist/services/ProjectServices.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-router.route("/addProject").get(addProject);
-function addProject(req, res) {
-    res.json("hello world!");
-}
-export { router };
-//# sourceMappingURL=ProjectServices.js.map
