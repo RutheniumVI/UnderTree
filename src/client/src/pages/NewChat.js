763cae21a7ca455f50764a40dafcfe6d1485c88a
qureshe00 <83733452+qureshe00@users.noreply.github.com>
2023-02-12 21:47:47 -0500

    add rooms

--- src/client/src/pages/NewChat.js
+++ src/client/src/pages/NewChat.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import io from 'socket.io-client';
+import { useState } from 'react';
 
 const socket = io.connect("http://localhost:8001");
 
 function NewChat() {
+
+    const [userName, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+
+    const joinRoom = () => {
+        if (userName != "" && room !=""){
+            socket.emit("join_room", room) //room is sent as "data" in backend
+            console.log(`User with socket ID ${socket.id} and username ${userName} joined the room`)
+        }
+    };
+
   return (
-    <div>newChat</div>
+    <div>
+        <h3>Join a Chat</h3>
+        <input type="text" 
+        placeholder='John..'
+        onChange={(event)=> {
+            setUsername(event.target.value);
+        }}/>
+        <input type="text" 
+        placeholder='RoomID..'
+        onChange={(event)=> {
+            setRoom(event.target.value);
+        }}/>
+        <div>
+            <button onClick={joinRoom}>CLICK ME</button>
+        </div>
+    </div>
   )
 }
 
--- src/server/src/UnderTree.ts
+++ src/server/src/UnderTree.ts
@@ -38,6 +38,10 @@ const io = new Server(chatserver, {
 io.on("connection", (socket)=> {
   console.log(`User Connected: ${socket.id}`);
 
+  socket.on("join_room", (data)=>{
+    socket.join(data)
+  })
+
   socket.on("disconnect", ()=> {
     console.log("User Disconnected", socket.id);
   });
