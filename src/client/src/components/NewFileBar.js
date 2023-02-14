d6627e29f5b773c5ab209594e367b025e0f679a4
qureshe00 <83733452+qureshe00@users.noreply.github.com>
2023-02-14 01:53:57 -0500

    add checkbox

--- src/client/src/components/NewFileBar.js
+++ src/client/src/components/NewFileBar.js
@@ -7,14 +7,23 @@ import '../Styles/NewFileBar.css'
 function NewFileBar() {
     const [filesList, setFilesList] = useState([{name:"file1"},{name:"file2"},{name:"file3"}]);
     const [foldersList, setFoldersList] = useState([{name:"folder1", files:[{name:"file1"},{name:"file2"},{name:"file3"}]},{name:"folder2"},{name:"folder3"}])
+    const [active, setActive] = useState(true);
+
+    function handleClick(){
+        //setActive(!active)
+        console.log("clicked")
+    }
   return (
     <div className='card my-card'>
         <div className='card-header'>
-            <button type="button" className="btn btn-light btn-lg my-btn">
+            <button type="button" className="btn btn-light btn-lg">
                 <i className="bi bi-file-earmark-plus"></i>
             </button> 
-            <button type="button" className="btn btn-light btn-lg my-btn">
+            <button type="button" className="btn btn-light btn-lg">
                 <i className="bi bi-cloud-upload"></i>
+            </button>  
+            <button type="button" className="btn btn-light btn-lg">
+                <i class="bi bi-folder-plus"></i>
             </button>  
         </div>
     <ProSidebarProvider>
@@ -23,12 +32,23 @@ function NewFileBar() {
                 {foldersList.map((folder, x) => {
                     return <SubMenu label={folder.name}> 
                         {foldersList[x].files?.map((file) => {
-                        return <MenuItem> {file.name} </MenuItem>
+                        return <MenuItem onClick={handleClick}> 
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"/>
+                            <label className="form-check-label" for="inlineCheckbox1">{file.name}</label>
+                        </div>
+                        <button type="button" className="btn btn-light my-btn"><i class="bi bi-three-dots-vertical"></i></button>    
+                        </MenuItem>
                 })}
                     </SubMenu>
                 })}
                 {filesList.map((file) => {
-                    return <MenuItem> {file.name} </MenuItem>
+                    return <MenuItem> 
+                    <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"/>
+                            <label className="form-check-label" for="inlineCheckbox1">{file.name}</label>
+                    </div>
+                    <button type="button" className="btn btn-light"><i class="bi bi-three-dots-vertical"></i></button></MenuItem>
                 })}
             </Menu>
         </Sidebar>
