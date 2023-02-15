7ccbceeb5c468a48f4e70fa2f9c5073e4ecc77ac
Veerash Palanichamy <veeresh.532000@gmail.com>
2023-02-14 22:10:30 -0500

    adding basic file api

--- src/client/src/pages/Dev.js
+++ src/client/src/pages/Dev.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Editor from '../components/Editor';
+// import Editor from '../components/Editor';
 import LiveUsers from '../components/LiveUsers';
 import Chat from './Chat';
 import Compiler from '../components/Compiler';
@@ -8,6 +8,8 @@ import Logout from '../components/Logout';
 import CreateProject from '../components/CreateProject';
 import Commit from '../components/Commit';
 import Collab from '../components/Collab';
+import { Button } from 'bootstrap';
+import axios from 'axios';
 
 function Dev() {
   return (
@@ -17,13 +19,56 @@ function Dev() {
         <div class="row justify-content-start">
           <div class="col">
             <LiveUsers/>
-            <Editor/>
-            <Chat/>
+            {/* <Editor/> */}
+            {/* <Chat/> */}
             <Login/>
             <Logout/>
             <CreateProject/>
             <Commit/>
             <Collab/>
+            <button onClick={() => {
+              axios.post("http://localhost:8000/api/file/addFile", {
+                  owner: "RutheniumVI",
+                  projectName: "veetestrepo",
+                  fileName: "random.tex",
+                  userName: "veerash"
+              }, {
+                withCredentials: true,
+              }).then((res) => {
+                console.log(res.data)
+              }).catch((error) => {
+                console.error(`Error Adding user to modified`);
+              });
+            }}>Create File</button>
+
+            <button onClick={() => {
+              axios.post("http://localhost:8000/api/file/renameFile", {
+                  owner: "RutheniumVI",
+                  projectName: "veetestrepo",
+                  fileName: "random.tex",
+                  userName: "veerash",
+                  newFileName: "rando1.tex"
+              }, {
+                withCredentials: true,
+              }).then((res) => {
+                console.log(res.data)
+              }).catch((error) => {
+                console.error(`Error Adding user to modified`);
+              });
+            }}>Rename File</button>
+
+            <button onClick={() => {
+              axios.post("http://localhost:8000/api/file/getFiles", {
+                  owner: "RutheniumVI",
+                  projectName: "veetestrepo"
+              }, {
+                withCredentials: true,
+              }).then((res) => {
+                console.log(res.data)
+              }).catch((error) => {
+                console.error(`Error Adding user to modified`);
+              });
+            }}>Rename File</button>
           </div>
           <div class="col">
             <Compiler/>
