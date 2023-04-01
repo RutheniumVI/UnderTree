806b39042ed817a80fdae05d5a413943f911cd82
Kevin Kannammalil <kevinrk2000@gmail.com>
2023-04-01 00:44:04 -0400

    made help modal

--- src/client/src/components/Navbar.js
+++ src/client/src/components/Navbar.js
@@ -55,7 +55,7 @@ function Navbar() {
                     }
                     </li>}
                     <li className="nav-item mx-2">
-                    <a className="nav-link" href="#">Help</a>
+                    <a className="nav-link" data-bs-toggle="modal" data-bs-target="#helpModal" href="#">Help</a>
                     </li>
                 </ul>
                 {localStorage.getItem("username") === null ? 
@@ -64,6 +64,32 @@ function Navbar() {
                     </a> 
                     : 
                     <button type="button" className="btn btn-primary mx-2" onClick={handleLogout}>Log Out</button>}
+                </div>
+            </div>
+
+            <div className="modal fade" id="helpModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="helpModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-scrollable">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="helpModalLabel"><strong>FAQ</strong></h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" id='helpModalClose'></button>
+                    </div>
+                    <div className="modal-body">
+                        <h5><strong>What is UnderTree?</strong></h5>
+                        <p class="font-weight-light">UnderTree is a Latex Editor integrated heavily with GitHub that allows you to manage your projects and collaborate with your team members.</p>
+                        <h5><strong>How do I get started?</strong></h5>
+                        <p class="font-weight-light">To get started, simply log in with your GitHub account. You will be redirected to the projects page where you can create a new project or import an existing one.</p>
+                        <h5><strong>How do I remove collaborators from my project?</strong></h5>
+                        <p class="font-weight-light">To remove collaborators from your project, click on the edit button on the project page. You can click on the collaborator you want to remove and then press "Save Changes".</p>
+                        <h5><strong>If I delete the project on UnderTree, will it delete the repository in the GitHub too?</strong></h5>
+                        <p class="font-weight-light">No, deleting the project on UnderTree will not delete the repository in the GitHub</p>
+                        <h5><strong>Will there be GitLab support as well?</strong></h5>
+                        <p class="font-weight-light">Yes, we are planning to add GitLab support in the future!</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                    </div>
                 </div>
             </div>
 
